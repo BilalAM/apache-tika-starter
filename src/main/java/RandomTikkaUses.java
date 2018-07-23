4d2766b76f82edc1b229c04ac30c2c221a281345
Bilal Asif Mirza <bilalam@localhost.localdomain>
2018-07-23 22:16:18 +0500

    random utils of tikka

---
+++ src/main/java/RandomTikkaUses.java
@@ -0,0 +1,46 @@
+import org.apache.tika.Tika;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RandomTikkaUses {
+
+
+    private static final Tika TIKA = new Tika();
+
+    public static Map<String, String> getFileMetaData(File file) throws Exception {
+        Map<String, String> metaDataPairs = new HashMap<>();
+        Metadata metadata = new Metadata();
+        TIKA.parse(file, metadata);
+        for (String metadataKey : metadata.names()) {
+            metaDataPairs.put(metadataKey, metadata.get(metadataKey));
+        }
+        return metaDataPairs;
+    }
+
+
+    public static String getFileContentType(File file) throws Exception {
+        return TIKA.detect(file);
+    }
+
+    public String getFileContent(File file) throws Exception {
+        Parser parser = new AutoDetectParser();
+        InputStream stream = TikaInputStream.get(Paths.get(file.toURI()));
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext parseContext = new ParseContext();
+
+        parser.parse(stream, handler, metadata, parseContext);
+
+        return handler.toString();
+    }
+}
