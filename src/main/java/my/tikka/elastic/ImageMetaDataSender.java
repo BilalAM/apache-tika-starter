e73366f37dd10a8abdbf62b8dde319dad0da32b7
BilalAM <bilal.asif.97@gmail.com>
2018-07-24 13:39:52 +0500

    image sender

---
+++ src/main/java/my/tikka/elastic/ImageMetaDataSender.java
@@ -0,0 +1,8 @@
+package my.tikka.elastic;
+
+/**
+ *  A wrapper class to create the metadata of an image (jpg , jpeg or png) to easily send it to kibana
+ */
+public class ImageMetaDataSender {
+
+}
