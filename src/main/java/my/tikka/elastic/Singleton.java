5ca1c2eef5be73bd518391dae37ed17548f84442
BilalAM <bilal.asif.97@gmail.com>
2018-07-24 13:41:25 +0500

    code extracted from previous repo

--- src/main/java/my/tikka/elastic/Singleton.java
+++ src/main/java/my/tikka/elastic/Singleton.java
@@ -1,4 +1,24 @@
 package my.tikka.elastic;
 
 public class Singleton {
+    private static TransportClient CLIENT_INSTANCE;
+
+    private Singleton() {
+
+    }
+
+    public static TransportClient getTransportClient() throws UnknownHostException {
+        if (CLIENT_INSTANCE == null) {
+            CLIENT_INSTANCE = new PreBuiltTransportClient(getSettings()).addTransportAddress(
+                    new TransportAddress(InetAddress.getByName("localhost"), 9300));
+        } else {
+            return CLIENT_INSTANCE;
+        }
+        return CLIENT_INSTANCE;
+    }
+
+    private static Settings getSettings() {
+        return Settings.builder().put("client.transport.sniff", true).build();
+    }
+
 }
