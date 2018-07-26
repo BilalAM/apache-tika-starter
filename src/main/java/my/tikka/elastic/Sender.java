d71a8b60bc72606b4d8d667f1313d7b91b9ceb9b
BilalAM <bilal.asif.97@gmail.com>
2018-07-26 23:20:31 +0500

    edited to have dynamic documents and mapper strategy

--- src/main/java/my/tikka/elastic/Sender.java
+++ src/main/java/my/tikka/elastic/Sender.java
@@ -1,9 +1,14 @@
 package my.tikka.elastic;
 
 import com.google.gson.Gson;
+import my.tikka.mappings.Document;
+import my.tikka.mappings.Mapper;
+import my.tikka.mappings.WordMapper;
 import org.apache.commons.io.FileUtils;
 import org.apache.tika.metadata.Metadata;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.json.simple.JSONObject;
 import tikka.RandomTikkaUtils;
 
 import java.io.File;
@@ -11,27 +16,30 @@ import java.util.Map;
 
 public class Sender {
 
-    private static void sendIndexMeta(Map<String, String> metadata, String typeName) {
+    private static void sendIndexMeta(Document document, String typeName) {
         try {
-
-
+            String responseDocument = new Gson().toJson(document);
             IndexResponse response = Singleton
                     .getTransportClient()
-                    .prepareIndex("filesystem-pdfs", typeName)
-                    .setSource(metadata)
+                    .prepareIndex("filesystem-word", typeName)
+                    .setSource(responseDocument, XContentType.JSON)
                     .get();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void sendData(String directoryPath, String typeName, String... extensions) {
+    public static void sendData(Mapper mappingStrategy, Document documentStrategy, String directoryPath, String typeName, String... extensions) {
         try {
             for (File _file : FileUtils.listFiles(new File(directoryPath), extensions, true)) {
-                sendIndexMeta(RandomTikkaUtils.getFileMetaData(_file), typeName);
+                documentStrategy = (Document) mappingStrategy.mapDocumentOf(_file);
+                sendIndexMeta(documentStrategy, typeName);
+                System.out.println("data sent...");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+
 }
