94cc2be54e6f3027074d4ae1820a9e36996c909a
BilalAM <bilal.asif.97@gmail.com>
2018-08-12 16:33:56 +0500

    refactored package names to core

--- src/main/java/my/tikka/mappings/Document.java
+++
@@ -1,4 +0,0 @@
-package my.tikka.mappings;
-
-public interface Document {
-}
--- src/main/java/my/tikka/mappings/Mapper.java
+++
@@ -1,8 +0,0 @@
-package my.tikka.mappings;
-
-import java.io.File;
-
-public interface Mapper<T> {
-
-    T mapDocumentOf(File _file);
-}
