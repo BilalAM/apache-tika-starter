138a9490212657110f9c4a16a4d4aba437f28e57
BilalAM <bilal.asif.97@gmail.com>
2018-09-04 04:21:32 +0500

    image mapper changes

--- src/main/java/tikka/core/mappings/ImageMapper.java
+++ src/main/java/tikka/core/mappings/ImageMapper.java
@@ -3,8 +3,11 @@ package tikka.core.mappings;
 import org.apache.tika.metadata.Metadata;
 import org.joda.time.DateTime;
 import tikka.RandomTikkaUtils;
+import tikka.core.abstractDocuments.ImageDocument;
 
 import java.io.File;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class ImageMapper implements Mapper {
 
@@ -12,23 +15,26 @@ public class ImageMapper implements Mapper {
 
     @Override
     public ImageDocument mapDocumentOf(File _file) {
-        imageDocument = new ImageDocument();
         try {
+            Path filePath = Paths.get(_file.toURI());
+            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            imageDocument = new ImageDocument();
+
             Metadata metadata = RandomTikkaUtils.getMetaData(_file);
             imageDocument.setResourceName(metadata.get("resourceName"));
             imageDocument.setContentLength(Long.parseLong(metadata.get("Content-Length")));
             imageDocument.setImageHeight(Long.parseLong(metadata.get("tiff:ImageLength")));
             imageDocument.setImageWidth(Long.parseLong(metadata.get("tiff:ImageWidth")));
-            imageDocument.setModifiedDate(DateTime.parse(metadata.get("Last-Save-Date")).toString());
+            imageDocument.setModifiedDate(attributes.lastModifiedTime().toString());
             imageDocument.setContentType(metadata.get("Content-Type"));
-            imageDocument.setNumberOfTables(Integer.parseInt(metadata.get("Number Of Tables")));
+            imageDocument.setModel(metadata.get("tiff:Model"));
+            imageDocument.setSoftware(metadata.get("tiff:Software"));
+            imageDocument.setCreationDate(attributes.creationTime().toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return imageDocument;
     }
-
-
 
 
 }
