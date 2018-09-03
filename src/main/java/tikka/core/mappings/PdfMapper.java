0c3c5c6d66c6974f73cdfcb1640486da439e8f1b
BilalAM <bilal.asif.97@gmail.com>
2018-09-04 04:21:12 +0500

    pdf mapper added

---
+++ src/main/java/tikka/core/mappings/PdfMapper.java
@@ -0,0 +1,10 @@
+package tikka.core.mappings;
+
+import java.io.File;
+
+public class PdfMapper implements Mapper {
+    @Override
+    public PdfMapper mapDocumentOf(File _file) {
+        return null;
+    }
+}
